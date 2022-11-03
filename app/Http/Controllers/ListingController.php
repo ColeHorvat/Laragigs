26406bf4154acbee472d53485802901a4827a72c
Cole <ColeHorvat@gmail.com>
2022-11-02 18:36:17 -0700

    Added tag filter

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -8,9 +8,10 @@ use Illuminate\Http\Request;
 class ListingController extends Controller {
     // Get and show all listings
     public function index() {
+        /* dd(request('tag')); */
         return view('listings.index', [
             'heading' => 'Latest Listings',
-            'listings' => Listing::all()
+            'listings' => Listing::latest()->filter(request(['tag']))->get(),
         ]);
     }
 
--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -5,7 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Listing extends Model
-{
+class Listing extends Model {
     use HasFactory;
+
+    public function scopeFilter($query, array $filters) {
+        // dd($filters['tag']);
+
+        if ($filters['tag'] ?? false) {
+            $query->where('tags', 'like', '%' . request('tag') . '%');
+        }
+    }
 }
